388f2cc23c539bd9f89e30c5f89fcba27e341b0c
Tomás de Sá <61152929+tomasdesa@users.noreply.github.com>
2024-12-10 16:48:20 +0000

    Update config.py

--- app/app_config/config.py
+++ app/app_config/config.py
@@ -33,7 +33,7 @@ class ConfService:
     secret_key = "secret_key"
 
     #service_url = "http://127.0.0.1:5000/"
-    service_url = "https://registry.serviceproviders.eudiw.dev/"
+    service_url = "https://trustedlist.serviceproviders.eudiw.dev/"
 
     #trusted_CAs_path = "app\certs"
     trusted_CAs_path = "/etc/eudiw/pid-issuer/cert/"
