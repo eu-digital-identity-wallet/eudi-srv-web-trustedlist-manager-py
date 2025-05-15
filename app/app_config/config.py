051a965120dcd9bb6ed6dfcb36317d07eaaadfb1
Tomás de Sá <61152929+tomasdesa@users.noreply.github.com>
2025-05-15 15:51:23 +0100

    Update config.py

--- app/app_config/config.py
+++ app/app_config/config.py
@@ -139,4 +139,4 @@ class ConfService:
     eu_countries = [
     "AT", "BE", "BG", "HR", "CY", "CZ", "DK", "EE", "FI", "FR",
     "DE", "GR", "HU", "IE", "IT", "LV", "LT", "LU", "MT", "NL",
-    "PL", "PT", "RO", "SK", "SI", "ES", "SE","UT"]
+    "PL", "PT", "RO", "SK", "SI", "ES", "SE","UT", "EU"]
