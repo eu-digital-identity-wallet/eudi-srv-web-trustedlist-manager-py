9a238afc154df143b95f87917b3f74b2c796ea8a
Tomás de Sá <61152929+tomasdesa@users.noreply.github.com>
2025-05-22 12:34:34 +0100

    Update xmlGen.py

--- app/xml_gen/xmlGen.py
+++ app/xml_gen/xmlGen.py
@@ -448,10 +448,9 @@ def xml_gen_xml(user_info, dictFromDB_trusted_lists, tsp_data, service_data, tsl
         data_object_format=data_object_format,
         c14n_algorithm="http://www.w3.org/TR/2001/REC-xml-c14n-20010315",
         signature_algorithm=algorithms.SignatureMethod.ECDSA_SHA256,
-        methods=methods.enveloped
     )
 
-    signed_root = signer.sign(data=rootTemp, key=key, cert=cert)
+    signed_root = signer.sign(data=rootTemp, key=key, cert=cert, method=methods.enveloped)
     #verified_data = XMLVerifier().verify(signed_root)
 
     # with open ("teste.xml", "w") as file: 
@@ -790,10 +789,9 @@ def xml_gen_lotl_xml(user_info, tsl_list, dict_tsl_mom, log_id):
         data_object_format=data_object_format,
         c14n_algorithm="http://www.w3.org/TR/2001/REC-xml-c14n-20010315",
         signature_algorithm=algorithms.SignatureMethod.ECDSA_SHA256,
-        methods=methods.enveloped
     )
 
-    signed_root = signer.sign(data=rootTemp, key=key, cert=cert)
+    signed_root = signer.sign(data=rootTemp, key=key, cert=cert, method=methods.enveloped)
     #verified_data = XMLVerifier().verify(signed_root)
 
     # with open ("teste.xml", "w") as file: 
