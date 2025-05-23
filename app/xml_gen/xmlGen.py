91cd22e20ce9f592c41930fc027e4c79892f9124
Tomás de Sá <61152929+tomasdesa@users.noreply.github.com>
2025-05-23 12:22:44 +0100

    Update xmlGen.py

--- app/xml_gen/xmlGen.py
+++ app/xml_gen/xmlGen.py
@@ -804,14 +804,15 @@ def xml_gen_lotl_xml(user_info, tsl_list, dict_tsl_mom, log_id):
     tree.write(xml_data, encoding='utf-8', xml_declaration=True)
     xml_data.seek(0)
 
-    content=xml_data.read()
+    content=xml_data.read().decode('utf-8')
 
     content = re.sub(r'xmlns:ns0="([^"]+)"', r'xmlns="\1"', content)
 
-    content = re.sub(r'<ns0:([^>\s/]+)', r'<\1', content)
-    content = re.sub(r'</ns0:([^>\s]+)>', r'</\1>', content)
-
-    encoded_file = base64.b64encode(content).decode('utf-8')
+    # Remover todos os prefixos ns0 das tags
+    content = re.sub(r'<ns0:', r'<', content)
+    content = re.sub(r'</ns0:', r'</', content)
+    
+    encoded_file = base64.b64encode(content.encode('utf-8')).decode('utf-8')
 
 
     return encoded_file, thumbprint, xml_hash_before_sign
